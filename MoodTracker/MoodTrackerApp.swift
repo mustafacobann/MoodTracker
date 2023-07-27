19e3348f1dd91b3c09b041b03d0827798c257133
Mustafa Çoban <cobanium@icloud.com>
2023-07-27 17:20:05 +0300

    Create project

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,19 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Mustafa on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
